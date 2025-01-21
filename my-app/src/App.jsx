fc8810b6e3158501cc9241e982c0cf2a8ce971b2
queges <glennspirig@icloud.com>
2025-01-21 05:02:54 -0500

    my-app updated to mergeRequest

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+  import { useEffect, useState } from "react";
+  import { createClient } from "@supabase/supabase-js";
 
-function App() {
-  const [count, setCount] = useState(0)
+  const supabase = createClient("https://xesrjpwntzntpqvlfthg.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhlc3JqcHdudHpudHBxdmxmdGhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc0NTI1NzQsImV4cCI6MjA1MzAyODU3NH0.Kfcas3TjxxHXRtlT3MDaoQm_-yFWqWtWROI1ruuqItc");
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  function App() {
+    const [countries, setCountries] = useState([]);
 
-export default App
+    useEffect(() => {
+      getCountries();
+    }, []);
+
+    async function getCountries() {
+      const { data } = await supabase.from("countries").select();
+      setCountries(data);
+    }
+
+    return (
+      <ul>
+        {countries.map((country) => (
+          <li key={country.name}>{country.name}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  export default App;
